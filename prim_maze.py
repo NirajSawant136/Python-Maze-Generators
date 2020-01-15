e955d31767531513917b0d638a67e408f7c657d5
Niraj Sawant <48999124+NirajSawant136@users.noreply.github.com>
2020-01-15 22:48:48 +0530

    Add files via upload

---
+++ prim_maze.py
@@ -0,0 +1,98 @@
+import pygame
+import time
+import random
+
+WIDTH = 1200
+HEIGHT = 700
+fps = 30
+
+WHITE = (255,255,255)
+RED = (255 , 38 , 38)
+
+pygame.init()
+pygame.mixer.init()
+MAZE = pygame.display.set_mode((WIDTH , HEIGHT))
+clock = pygame.time.Clock()
+
+global width , height
+
+width , height = 75 , 45
+
+marked = []
+visited = []
+
+def buildMaze():
+	x , y = random.randint(0,width-1) , random.randint(0, height-1)
+	marked.append((x , y))
+	pygame.draw.rect(MAZE , WHITE , (x*15,y*15,15,15),0)
+	pygame.display.update()
+	visited = addNeighbors(x,y)
+
+	while len(visited) != 0:
+		time.sleep(0.1)
+		(x , y) = random.choice(visited)
+		pygame.draw.rect(MAZE , WHITE , (x*15,y*15,15,15),0)
+		pygame.display.update()
+		visited.remove((x , y))
+		marked.append((x , y))
+		visited = addNeighbors(x , y)
+		removeWall(x , y)
+
+def addNeighbors(x,y):
+	if not (x-2,y) in marked and not (x-2,y) in visited and x-2 > 0:
+		visited.append((x-2 , y))
+		pygame.draw.rect(MAZE , RED , ((x-2)*15,y*15,15,15),0)
+		pygame.display.update()
+
+	if not (x+2,y) in marked and not (x+2,y) in visited and x+2 < width:
+		visited.append((x+2,y))
+		pygame.draw.rect(MAZE , RED , ((x+2)*15,y*15,15,15),0)
+		pygame.display.update()
+
+	if not (x,y-2) in marked and not (x,y-2) in visited and y-2 > 0:
+		visited.append((x,y-2))
+		pygame.draw.rect(MAZE , RED , (x*15,(y-2)*15,15,15),0)
+		pygame.display.update()
+
+	if not (x,y+2) in marked and not (x,y+2) in visited and y+2 < height:
+		visited.append((x,y+2))
+		pygame.draw.rect(MAZE , RED , (x*15,(y+2)*15,15,15),0)
+		pygame.display.update()
+
+	return visited
+
+def removeWall(x , y):
+	randList = []
+	if (x-2,y) in marked:
+		randList.append((x-1,y))
+	
+	if (x+2,y) in marked:
+		randList.append((x+1,y))
+
+	if (x,y-2) in marked:
+		randList.append((x,y-1))
+
+	if (x,y+2) in marked:
+		randList.append((x,y+1))
+
+	if len(randList) != 0:
+		(x,y) = random.choice(randList)
+		pygame.draw.rect(MAZE , WHITE , (x*15,y*15,15,15),0)
+		pygame.display.update()
+
+	return
+
+
+buildMaze()
+
+run = True
+while run:
+	clock.tick(fps)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			run = False
+
+		if event.type == ord('q'):
+			pygame.quit()
+			run = False
---
+++ recursive_backtracker.py
@@ -0,0 +1,118 @@
+import random
+from tkinter import *
+import pygame
+import time
+
+WIDTH = 685
+HEIGHT = 685
+fps = 30
+
+WHITE = (255,255,255)
+RED = (255 , 38 , 38)
+
+pygame.init()
+pygame.mixer.init()
+MAZE = pygame.display.set_mode((WIDTH , HEIGHT))
+clock = pygame.time.Clock()
+
+def buildMaze(maze , size):
+        x = 1
+        y = 1
+        count = 1
+        soln = [(x,y-1),(x,y)]
+        stack = [(x,y)]
+        randTile( maze , stack , soln , size )
+
+def stopCarving(maze , size):
+        count = 0
+        for i in range(1,size,2):
+                for j in range(1,size,2):
+                        if maze[i][j] == 1:
+                                count += 1
+
+        return count != ((size-1)//2)**2
+                                       
+def randTile(maze , stack , soln , size):
+        if stopCarving(maze , size) and len(stack) != 0:
+                
+                x , y = stack[len(stack)-1]
+                maze[x][y] = 1
+                time.sleep(0.1)
+                pygame.draw.rect(MAZE , WHITE , (x*15,y*15,15,15),0)
+                pygame.display.update()
+                randList = []
+                if x + 2 < size-1:
+                        if maze[x+2][y] == 0:
+                                randList.append(1)
+                if x - 2 > 0:
+                        if maze[x-2][y] == 0:
+                                randList.append(2)
+                if y + 2 < size-1:
+                        if maze[x][y+2] == 0:
+                                randList.append(3)
+                if y - 2 > 0:
+                        if maze[x][y - 2] == 0:
+                                randList.append(4)
+                
+                if len(randList) == 0:
+                        stack.pop()
+                        if (size-2,size-2) not in soln:
+                                soln.pop()
+                                soln.pop()
+                        randTile(maze , stack ,soln ,  size)
+                        
+                else:
+                        options = random.choice(randList)
+                        if options == 1:
+                                x = x + 2
+                                stack.append((x,y))
+                                if (size-2,size-2) not in soln:
+                                        soln.append((x-1,y))
+                                        soln.append((x,y))
+                                maze[x-1][y] = 1
+                                pygame.draw.rect(MAZE , WHITE , ((x-1)*15,y*15,15,15),0)
+                                pygame.display.update()
+                                randTile(maze , stack , soln , size)
+                        elif options == 2:
+                                x = x - 2
+                                stack.append((x,y))
+                                if (size-2,size-2) not in soln:
+                                        soln.append((x+1,y))
+                                        soln.append((x,y))
+                                maze[x+1][y] = 1
+                                pygame.draw.rect(MAZE , WHITE , ((x+1)*15,y*15,15,15),0)
+                                pygame.display.update()
+                                randTile(maze , stack , soln , size)
+                        elif options == 3:
+                                y = y + 2
+                                stack.append((x,y))
+                                if (size-2,size-2) not in soln:
+                                        soln.append((x,y-1))
+                                        soln.append((x,y))
+                                maze[x][y-1] = 1
+                                pygame.draw.rect(MAZE , WHITE , (x*15,(y-1)*15,15,15),0)
+                                pygame.display.update()
+                                randTile(maze , stack , soln , size)
+                        elif options == 4:
+                                y = y - 2
+                                stack.append((x,y))
+                                if (size-2,size-2) not in soln:
+                                        soln.append((x,y+1))
+                                        soln.append((x,y))
+                                maze[x][y+1] = 1
+                                pygame.draw.rect(MAZE , WHITE , (x*15,(y+1)*15,15,15),0)
+                                pygame.display.update()
+                                randTile(maze , stack , soln , size)
+
+
+n = 45
+maze = [[0 for i in range(n)]for j in range(n)]
+buildMaze(maze , n)
+
+run = True
+while run:
+        clock.tick(fps)
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        pygame.quit()
+                        run = False
